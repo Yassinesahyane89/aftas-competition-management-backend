27bac48dec204c148535cf9f99b3ae7acc121ed7
yassine sahyane <62175613+Yassinesahyane89@users.noreply.github.com>
2023-12-17 22:56:05 +0100

    add attribute with there validation and toLevel methode for mapping in LevelRequestDTO record

---
+++ src/main/java/com/example/demo/web/DTO/request/LevelRequestDTO.java
@@ -0,0 +1,26 @@
+package com.example.demo.web.DTO.request;
+
+import com.example.demo.entity.Level;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+
+public record LevelRequestDTO(
+        @NotNull(message = "description is required")
+        @NotBlank(message = "description is required")
+        @Size(min = 3, max = 255, message = "description must be between 3 and 255 characters")
+        String description,
+
+        @NotNull(message = "point is required")
+        @NotBlank(message = "point is required")
+        @Min(value = 0, message = "point must be greater than 0")
+        Integer point
+) {
+    public Level toLevel() {
+        return Level.builder()
+                .description(description)
+                .point(point)
+                .build();
+    }
+}
