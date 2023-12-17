6867fd5f16f56380737e66f979583329fc3a80a4
yassine sahyane <62175613+Yassinesahyane89@users.noreply.github.com>
2023-12-17 18:57:03 +0100

    add signature methode for LevelService interface

--- src/main/java/com/example/demo/service/LevelService.java
+++ src/main/java/com/example/demo/service/LevelService.java
@@ -1,4 +1,13 @@
 package com.example.demo.service;
 
+import com.example.demo.entity.Level;
+
+import java.util.List;
+
 public interface LevelService {
+    Level getLevelById(Long id);
+    List<Level> getAllLevels();
+    Level addLevel(Level level);
+    Level updateLevel(Level level, Long id);
+    void deleteLevel(Long id);
 }
