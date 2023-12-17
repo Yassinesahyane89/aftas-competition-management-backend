a5bca06b0d121b6ade36e9a186d6eb6be5974d88
yassine sahyane <62175613+Yassinesahyane89@users.noreply.github.com>
2023-12-17 22:31:29 +0100

    add CustomExceptionHandler class && handleOperationException for custom return message for OperationException error

--- src/main/java/com/example/demo/handler/exception/CustomExceptionHandler.java
+++ src/main/java/com/example/demo/handler/exception/CustomExceptionHandler.java
@@ -1,9 +1,15 @@
 package com.example.demo.handler.exception;
 
+import com.example.demo.handler.response.ResponseMessage;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 
 @ControllerAdvice
 public class CustomExceptionHandler {
-
-
+    @ExceptionHandler(OperationException.class)
+    public ResponseEntity<ResponseMessage> handleOperationException(OperationException e){
+        ResponseMessage responseMessage = new ResponseMessage(e.getMessage());
+        return ResponseEntity.badRequest().body(responseMessage);
+    }
 }
