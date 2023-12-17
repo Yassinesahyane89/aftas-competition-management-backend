cec7a34cf3f596dae9b4facdbca5569c29912357
yassine sahyane <62175613+Yassinesahyane89@users.noreply.github.com>
2023-12-17 18:50:31 +0100

    add attribute to Level entity

--- src/main/java/com/example/demo/entity/Level.java
+++ src/main/java/com/example/demo/entity/Level.java
@@ -1,13 +1,40 @@
 package com.example.demo.entity;
 
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.util.Date;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
+@Entity
+@EntityListeners(AuditingEntityListener.class)
 public class Level {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private Integer code;
+
+    private String description;
+
+    private Integer point;
+
+    @CreationTimestamp
+    @Temporal(TemporalType.TIMESTAMP)
+    @Column(nullable = false)
+    private Date createdAt;
+
+    @UpdateTimestamp
+    @Temporal(TemporalType.TIMESTAMP)
+    @Column(nullable = false)
+    private Date updatedAt;
 }
