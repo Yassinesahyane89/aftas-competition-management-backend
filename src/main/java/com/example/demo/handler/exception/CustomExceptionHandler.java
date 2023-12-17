351daa3f4667483b1f1b34cbc154524f38b3d301
yassine sahyane <62175613+Yassinesahyane89@users.noreply.github.com>
2023-12-17 22:35:03 +0100

    add handleValidationExceptions in CustomExceptionHandler class for custom return message for MethodArgumentNotValidException exception

--- src/main/java/com/example/demo/handler/exception/CustomExceptionHandler.java
+++ src/main/java/com/example/demo/handler/exception/CustomExceptionHandler.java
@@ -2,8 +2,14 @@ package com.example.demo.handler.exception;
 
 import com.example.demo.handler.response.ResponseMessage;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 @ControllerAdvice
 public class CustomExceptionHandler {
@@ -18,4 +24,15 @@ public class CustomExceptionHandler {
         ResponseMessage responseMessage = new ResponseMessage(e.getMessage());
         return ResponseEntity.badRequest().body(responseMessage);
     }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity handleValidationExceptions(MethodArgumentNotValidException e){
+        Map<String, List<String>> errors = e.getBindingResult()
+                .getFieldErrors()
+                .stream()
+                .collect(Collectors.groupingBy(FieldError::getField, Collectors.mapping(FieldError::getDefaultMessage, Collectors.toList())));
+
+        return ResponseEntity.badRequest().body(errors);
+    }
+
 }
