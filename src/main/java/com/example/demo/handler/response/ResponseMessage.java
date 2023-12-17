a5a40749eb37aab866a170ffafb5c1f723e22b3c
yassine sahyane <62175613+Yassinesahyane89@users.noreply.github.com>
2023-12-17 20:20:10 +0100

    add attribute in ResponseMessage class and constructors

--- src/main/java/com/example/demo/handler/response/ResponseMessage.java
+++ src/main/java/com/example/demo/handler/response/ResponseMessage.java
@@ -1,4 +1,18 @@
 package com.example.demo.handler.response;
 
+import lombok.Data;
+
+@Data
 public class ResponseMessage {
+    private String message;
+    private Object data;
+
+    public ResponseMessage(String message, Object data) {
+        this.message = message;
+        this.data = data;
+    }
+
+    public ResponseMessage(String message) {
+        this.message = message;
+    }
 }
