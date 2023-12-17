3b756f8409b97f3a7bcef9233b6336eac2e6b4b1
yassine sahyane <62175613+Yassinesahyane89@users.noreply.github.com>
2023-12-17 18:58:33 +0100

    creat class implementation for LevelService

---
+++ src/main/java/com/example/demo/service/Impl/LevelServiceImpl.java
@@ -0,0 +1,33 @@
+package com.example.demo.service.Impl;
+
+import com.example.demo.entity.Level;
+import com.example.demo.service.LevelService;
+
+import java.util.List;
+
+public class LevelServiceImpl implements LevelService {
+    @Override
+    public Level getLevelById(Long id) {
+        return null;
+    }
+
+    @Override
+    public List<Level> getAllLevels() {
+        return null;
+    }
+
+    @Override
+    public Level addLevel(Level level) {
+        return null;
+    }
+
+    @Override
+    public Level updateLevel(Level level, Long id) {
+        return null;
+    }
+
+    @Override
+    public void deleteLevel(Long id) {
+
+    }
+}
