96d34876ac83e51ef857474c98362075cb8601cd
yassine sahyane <62175613+Yassinesahyane89@users.noreply.github.com>
2023-12-17 19:05:03 +0100

    injecter LevelRepository in LevelServiceImpl

--- src/main/java/com/example/demo/service/Impl/LevelServiceImpl.java
+++ src/main/java/com/example/demo/service/Impl/LevelServiceImpl.java
@@ -1,11 +1,17 @@
 package com.example.demo.service.Impl;
 
 import com.example.demo.entity.Level;
+import com.example.demo.repository.LevelRepository;
 import com.example.demo.service.LevelService;
 
 import java.util.List;
 
 public class LevelServiceImpl implements LevelService {
+    private LevelRepository levelRepository;
+
+    public LevelServiceImpl(LevelRepository levelRepository) {
+        this.levelRepository = levelRepository;
+    }
     @Override
     public Level getLevelById(Long id) {
         return null;
