b266704c81d458fbb8ef72a28b8bedd7a516f0e6
yassine sahyane <62175613+Yassinesahyane89@users.noreply.github.com>
2023-12-17 23:21:04 +0100

    add methode getAllLevels in LevelController

--- src/main/java/com/example/demo/web/rest/controller/LevelController.java
+++ src/main/java/com/example/demo/web/rest/controller/LevelController.java
@@ -1,9 +1,12 @@
 package com.example.demo.web.rest.controller;
 
+import com.example.demo.entity.Level;
+import com.example.demo.handler.response.ResponseMessage;
 import com.example.demo.service.LevelService;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/levels")
@@ -14,5 +17,16 @@ public class LevelController {
 
     public LevelController(LevelService levelService) {
         this.levelService = levelService;
+    }
+
+    // get all levels
+    @GetMapping
+    public ResponseEntity getAllLevels() {
+        List<Level> levels = levelService.getAllLevels();
+        if(levels.isEmpty()) {
+            return ResponseMessage.notFound("Levels not found");
+        }else {
+            return ResponseMessage.ok(levels, "Success");
+        }
     }
 }
