7a420f5fd9eb5f2568570088dc9b905dcd20d6c2
yassine sahyane <62175613+Yassinesahyane89@users.noreply.github.com>
2023-12-18 20:10:19 +0100

    add methode findByName in FishRepository

--- src/main/java/com/example/demo/repository/FishRepository.java
+++ src/main/java/com/example/demo/repository/FishRepository.java
@@ -4,5 +4,5 @@ import com.example.demo.entity.Fish;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface FishRepository extends JpaRepository<Fish, Long> {
-
+    Fish findByName(String name);
 }
