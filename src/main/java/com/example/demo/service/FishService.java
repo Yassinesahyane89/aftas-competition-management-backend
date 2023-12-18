f6ac7793fff64ee2b44e2cd35bc6fac4fe8b09fa
yassine sahyane <62175613+Yassinesahyane89@users.noreply.github.com>
2023-12-18 20:05:50 +0100

    add my methode signature in FishService interface

--- src/main/java/com/example/demo/service/FishService.java
+++ src/main/java/com/example/demo/service/FishService.java
@@ -1,4 +1,14 @@
 package com.example.demo.service;
 
+import com.example.demo.entity.Fish;
+
+import java.util.List;
+
 public interface FishService {
+    Fish getFishById(Long id);
+    List<Fish> getAllFishes();
+    Fish addFish(Fish fish);
+    Fish updateFish(Fish fish, Long id);
+    void deleteFish(Long id);
+
 }
