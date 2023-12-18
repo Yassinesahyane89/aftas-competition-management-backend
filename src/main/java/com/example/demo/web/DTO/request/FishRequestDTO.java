aae805f9a2b5d2791cbc21331c84a737e619c677
yassine sahyane <62175613+Yassinesahyane89@users.noreply.github.com>
2023-12-18 20:24:13 +0100

    add toFish to do mapping from FishRequestDTO to fish

--- src/main/java/com/example/demo/web/DTO/request/FishRequestDTO.java
+++ src/main/java/com/example/demo/web/DTO/request/FishRequestDTO.java
@@ -1,9 +1,18 @@
 package com.example.demo.web.DTO.request;
 
+import com.example.demo.entity.Fish;
+import com.example.demo.entity.Level;
+
 public record FishRequestDTO(
         String name,
         double averageWeight,
         Long levelId
 ) {
-
+     public static Fish toFish(FishRequestDTO fishRequestDTO) {
+      return  Fish.builder()
+              .name(fishRequestDTO.name())
+              .averageWeight(fishRequestDTO.averageWeight())
+              .level(Level.builder().id(fishRequestDTO.levelId()).build())
+            .build();
+     }
 }
