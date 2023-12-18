fb49564dec49be305961e330fb25ac3dc501a1f5
yassine sahyane <62175613+Yassinesahyane89@users.noreply.github.com>
2023-12-18 20:39:45 +0100

    add methode getAllFishes and it logic in FishController

--- src/main/java/com/example/demo/web/rest/controller/FishController.java
+++ src/main/java/com/example/demo/web/rest/controller/FishController.java
@@ -1,9 +1,17 @@
 package com.example.demo.web.rest.controller;
 
+import com.example.demo.entity.Fish;
+import com.example.demo.handler.response.ResponseMessage;
 import com.example.demo.service.FishService;
+import com.example.demo.web.DTO.response.FishResponseDTO;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/fishes")
@@ -13,5 +21,19 @@ public class FishController {
 
     public FishController(FishService fishService) {
         this.fishService = fishService;
+    }
+
+    @GetMapping
+    public ResponseEntity getAllFishes() {
+        List<Fish> fishes = fishService.getAllFishes();
+        if(fishes.isEmpty()) {
+            return ResponseMessage.notFound("Fish not found");
+        }else {
+            List<FishResponseDTO> fishResponseDTOS = new ArrayList<>();
+            for(Fish fish : fishes) {
+                fishResponseDTOS.add(FishResponseDTO.fromFish(fish));
+            }
+            return ResponseMessage.ok(fishResponseDTOS, "Success");
+        }
     }
 }
