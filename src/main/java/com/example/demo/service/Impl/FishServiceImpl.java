471416dd2865b52e877c77bdec82b842c9553653
yassine sahyane <62175613+Yassinesahyane89@users.noreply.github.com>
2023-12-18 20:06:42 +0100

    create FishServiceImpl clas implementation of FishService interface

---
+++ src/main/java/com/example/demo/service/Impl/FishServiceImpl.java
@@ -0,0 +1,33 @@
+package com.example.demo.service.Impl;
+
+import com.example.demo.entity.Fish;
+import com.example.demo.service.FishService;
+
+import java.util.List;
+
+public class FishServiceImpl implements FishService {
+    @Override
+    public Fish getFishById(Long id) {
+        return null;
+    }
+
+    @Override
+    public List<Fish> getAllFishes() {
+        return null;
+    }
+
+    @Override
+    public Fish addFish(Fish fish) {
+        return null;
+    }
+
+    @Override
+    public Fish updateFish(Fish fish, Long id) {
+        return null;
+    }
+
+    @Override
+    public void deleteFish(Long id) {
+
+    }
+}
