76973534a78e1c8b941dc6210a5a439a14b269b4
yassine sahyane <62175613+Yassinesahyane89@users.noreply.github.com>
2023-12-18 20:07:37 +0100

    inject fishRepository in FishServiceImpl

--- src/main/java/com/example/demo/service/Impl/FishServiceImpl.java
+++ src/main/java/com/example/demo/service/Impl/FishServiceImpl.java
@@ -1,11 +1,17 @@
 package com.example.demo.service.Impl;
 
 import com.example.demo.entity.Fish;
+import com.example.demo.repository.FishRepository;
 import com.example.demo.service.FishService;
 
 import java.util.List;
 
 public class FishServiceImpl implements FishService {
+    private FishRepository fishRepository;
+
+    public FishServiceImpl(FishRepository fishRepository) {
+        this.fishRepository = fishRepository;
+    }
     @Override
     public Fish getFishById(Long id) {
         return null;
