e068876150eba900446eebc44b16dee9592ab903
yassine sahyane <62175613+Yassinesahyane89@users.noreply.github.com>
2023-12-19 19:45:25 +0100

    create the declaration of methods in RankingService

--- src/main/java/com/example/demo/service/RankingService.java
+++ src/main/java/com/example/demo/service/RankingService.java
@@ -1,4 +1,16 @@
 package com.example.demo.service;
 
+import com.example.demo.entity.Ranking;
+
+import java.util.List;
+
 public interface RankingService {
+    Ranking getRankingByCompetitionCodeAndMemberNumber(String competitionCode, Long memberNumber);
+    List<Ranking> getAllRankings();
+    Ranking addRanking(Ranking ranking);
+    Ranking updateRanking(Ranking ranking, String competitionCode, Long memberNumber);
+    void deleteRanking(String competitionCode, Long memberNumber);
+    List<Ranking> getRankingsByCompetitionCode(String competitionCode);
+    List<Ranking> getRankingsByMemberNumber(Long memberNumber);
+
 }
