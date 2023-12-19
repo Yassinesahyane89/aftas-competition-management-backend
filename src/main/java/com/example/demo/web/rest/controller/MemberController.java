e534289e338b32e388ee86af0c90b495f9012368
yassine sahyane <62175613+Yassinesahyane89@users.noreply.github.com>
2023-12-19 10:25:59 +0100

    create and init MemberController

---
+++ src/main/java/com/example/demo/web/rest/controller/MemberController.java
@@ -0,0 +1,12 @@
+package com.example.demo.web.rest.controller;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/members")
+@CrossOrigin("*")
+public class MemberController {
+
+}
