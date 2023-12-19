17dd3a634f93ed4f590f428eaa6cafc85885e7d6
yassine sahyane <62175613+Yassinesahyane89@users.noreply.github.com>
2023-12-19 09:55:05 +0100

    add methode signature to MemberService interface

--- src/main/java/com/example/demo/service/MemberService.java
+++ src/main/java/com/example/demo/service/MemberService.java
@@ -1,4 +1,14 @@
 package com.example.demo.service;
 
+import com.example.demo.entity.Member;
+
+import java.util.List;
+
 public interface MemberService {
+    Member getMemberById(Long id);
+    List<Member> getAllMembers();
+    List<Member> findByFirstNameOrMembershipNumberOrFamilyName(String searchTerm);
+    Member addMember(Member member);
+    Member updateMember(Member member, Long id);
+    void deleteMember(Long id);
 }
