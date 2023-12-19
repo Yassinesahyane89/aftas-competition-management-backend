4d2a92ddf6732224ad4459103c00f1f7fac6ded6
yassine sahyane <62175613+Yassinesahyane89@users.noreply.github.com>
2023-12-19 20:47:41 +0100

    add findAllByCompetitionCode method in RankingRepository

--- src/main/java/com/example/demo/repository/RankingRepository.java
+++ src/main/java/com/example/demo/repository/RankingRepository.java
@@ -4,6 +4,9 @@ import com.example.demo.entity.RankId;
 import com.example.demo.entity.Ranking;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface RankingRepository extends JpaRepository<Ranking, RankId> {
     Ranking findAllByMemberMembershipNumberAndCompetitionCode(Long memberNumber, String competitionCode);
+    List<Ranking> findAllByCompetitionCode(String competitionCode);
 }
