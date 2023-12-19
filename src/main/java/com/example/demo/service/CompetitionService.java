4e88ea2271ef9a426732d688286039195f7fb354
yassine sahyane <62175613+Yassinesahyane89@users.noreply.github.com>
2023-12-19 18:33:21 +0100

    init CompetitionService by signature methode

--- src/main/java/com/example/demo/service/CompetitionService.java
+++ src/main/java/com/example/demo/service/CompetitionService.java
@@ -1,4 +1,19 @@
 package com.example.demo.service;
 
+import com.example.demo.entity.Competition;
+import com.example.demo.entity.Member;
+import com.example.demo.entity.Ranking;
+
+import java.util.List;
+
 public interface CompetitionService {
+    Competition getCompetitionById(Long id);
+    List<Competition> getAllCompetitions();
+    Competition addCompetition(Competition competition);
+    Competition updateCompetition(Competition competition, Long id);
+    void deleteCompetition(Long id);
+    Ranking registerMemberForCompetition(Ranking ranking);
+    List<Ranking> registerMembersForCompetition(List<Ranking> rankings);
+    List<Member> getMembersByCompetitionId(Long id);
+    List<Ranking> getRankingsByCompetitionId(Long id);
 }
