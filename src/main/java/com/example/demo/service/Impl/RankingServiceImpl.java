2bfecba96ef91d34931b4da6beb69cbdadcf0afe
yassine sahyane <62175613+Yassinesahyane89@users.noreply.github.com>
2023-12-19 19:46:31 +0100

    create RankingServiceImpl implementation of RankingService

---
+++ src/main/java/com/example/demo/service/Impl/RankingServiceImpl.java
@@ -0,0 +1,43 @@
+package com.example.demo.service.Impl;
+
+import com.example.demo.entity.Ranking;
+import com.example.demo.service.RankingService;
+
+import java.util.List;
+
+public class RankingServiceImpl implements RankingService {
+    @Override
+    public Ranking getRankingByCompetitionCodeAndMemberNumber(String competitionCode, Long memberNumber) {
+        return null;
+    }
+
+    @Override
+    public List<Ranking> getAllRankings() {
+        return null;
+    }
+
+    @Override
+    public Ranking addRanking(Ranking ranking) {
+        return null;
+    }
+
+    @Override
+    public Ranking updateRanking(Ranking ranking, String competitionCode, Long memberNumber) {
+        return null;
+    }
+
+    @Override
+    public void deleteRanking(String competitionCode, Long memberNumber) {
+
+    }
+
+    @Override
+    public List<Ranking> getRankingsByCompetitionCode(String competitionCode) {
+        return null;
+    }
+
+    @Override
+    public List<Ranking> getRankingsByMemberNumber(Long memberNumber) {
+        return null;
+    }
+}
