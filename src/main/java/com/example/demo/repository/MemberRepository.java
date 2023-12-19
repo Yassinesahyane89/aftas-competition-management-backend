21a00cc57dd05c8ec301bfba6ecb696075695115
yassine sahyane <62175613+Yassinesahyane89@users.noreply.github.com>
2023-12-19 10:00:30 +0100

    add query of findByMembershipNumberOrFirstNameOrFamilyName and it is logic

--- src/main/java/com/example/demo/repository/MemberRepository.java
+++ src/main/java/com/example/demo/repository/MemberRepository.java
@@ -2,6 +2,14 @@ package com.example.demo.repository;
 
 import com.example.demo.entity.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface MemberRepository extends JpaRepository<Member, Long> {
+    @Query( value =
+            "SELECT * FROM member WHERE membership_number = :searchTerm " +
+            "OR first_name LIKE %:searchTerm% OR family_name LIKE %:searchTerm%", nativeQuery = true)
+    List<Member> findByMembershipNumberOrFirstNameOrFamilyName(@Param("searchTerm") String searchTerm);
 }
