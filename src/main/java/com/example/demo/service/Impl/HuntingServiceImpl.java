57a338f2446b15f35aea4b34eb3a8b23a816884e
yassine sahyane <62175613+Yassinesahyane89@users.noreply.github.com>
2023-12-19 22:12:46 +0100

    inject MemberService && CompetitionService && RankingService && FishService in HuntingServiceImpl

--- src/main/java/com/example/demo/service/Impl/HuntingServiceImpl.java
+++ src/main/java/com/example/demo/service/Impl/HuntingServiceImpl.java
@@ -3,15 +3,25 @@ package com.example.demo.service.Impl;
 import com.example.demo.entity.Hunting;
 import com.example.demo.handler.exception.ResourceNotFountException;
 import com.example.demo.repository.HuntingRepository;
-import com.example.demo.service.HuntingService;
+import com.example.demo.service.*;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+@Service
 public class HuntingServiceImpl implements HuntingService {
     private final HuntingRepository huntingRepository;
+    private final MemberService memberService;
+    private final CompetitionService competitionService;
+    private final RankingService rankingService;
+    private final FishService fishService;
 
-    public HuntingServiceImpl(HuntingRepository huntingRepository) {
+    public HuntingServiceImpl(HuntingRepository huntingRepository, MemberService memberService, CompetitionService competitionService, RankingService rankingService, FishService fishService) {
         this.huntingRepository = huntingRepository;
+        this.memberService = memberService;
+        this.competitionService = competitionService;
+        this.rankingService = rankingService;
+        this.fishService = fishService;
     }
     @Override
     public Hunting getHuntingById(Long id) {
