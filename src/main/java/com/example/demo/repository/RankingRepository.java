595bc188d5b4702df439b030b5bc2e15c4d9be86
yassine sahyane <62175613+Yassinesahyane89@users.noreply.github.com>
2023-12-19 18:21:35 +0100

    add RankingRepository

---
+++ src/main/java/com/example/demo/repository/RankingRepository.java
@@ -0,0 +1,7 @@
+package com.example.demo.repository;
+
+import com.example.demo.entity.Ranking;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RankingRepository extends JpaRepository<Ranking, Long> {
+}
