1b618fabceb2c0397f3955a5fa7f1bda61434e4b
yassine sahyane <62175613+Yassinesahyane89@users.noreply.github.com>
2023-12-19 22:03:39 +0100

    add signature method to HuntingService

--- src/main/java/com/example/demo/service/HuntingService.java
+++ src/main/java/com/example/demo/service/HuntingService.java
@@ -1,4 +1,14 @@
 package com.example.demo.service;
 
+import com.example.demo.entity.Hunting;
+
+import java.util.List;
+
 public interface HuntingService {
+    Hunting getHuntingById(Long id);
+    Hunting addHuntingResult(Hunting hunting);
+    List<Hunting> getHuntingsByCompetition(Long competitionId);
+    List<Hunting> getHuntingsByCompetitionAndMember(Long competitionId, Long memberId);
+    Hunting updateHunting(Hunting hunting, Long id);
+    void deleteHunting(Long id);
 }
