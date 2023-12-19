a781ab8de7ed63f37231233ac930009c33ff8200
yassine sahyane <62175613+Yassinesahyane89@users.noreply.github.com>
2023-12-19 09:56:52 +0100

    inject MemberRepository in MemberServiceImpl

--- src/main/java/com/example/demo/service/Impl/MemberServiceImpl.java
+++ src/main/java/com/example/demo/service/Impl/MemberServiceImpl.java
@@ -1,11 +1,19 @@
 package com.example.demo.service.Impl;
 
 import com.example.demo.entity.Member;
+import com.example.demo.repository.MemberRepository;
 import com.example.demo.service.MemberService;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+@Service
 public class MemberServiceImpl implements MemberService {
+    private final MemberRepository memberRepository;
+
+    public MemberServiceImpl(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
+    }
     @Override
     public Member getMemberById(Long id) {
         return null;
