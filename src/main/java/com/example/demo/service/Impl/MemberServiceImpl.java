d3237c378eb41097b62aad3ea50cdf6ecaa56471
yassine sahyane <62175613+Yassinesahyane89@users.noreply.github.com>
2023-12-19 09:55:57 +0100

    create implementation of MemberService interface

---
+++ src/main/java/com/example/demo/service/Impl/MemberServiceImpl.java
@@ -0,0 +1,38 @@
+package com.example.demo.service.Impl;
+
+import com.example.demo.entity.Member;
+import com.example.demo.service.MemberService;
+
+import java.util.List;
+
+public class MemberServiceImpl implements MemberService {
+    @Override
+    public Member getMemberById(Long id) {
+        return null;
+    }
+
+    @Override
+    public List<Member> getAllMembers() {
+        return null;
+    }
+
+    @Override
+    public List<Member> findByFirstNameOrMembershipNumberOrFamilyName(String searchTerm) {
+        return null;
+    }
+
+    @Override
+    public Member addMember(Member member) {
+        return null;
+    }
+
+    @Override
+    public Member updateMember(Member member, Long id) {
+        return null;
+    }
+
+    @Override
+    public void deleteMember(Long id) {
+
+    }
+}
