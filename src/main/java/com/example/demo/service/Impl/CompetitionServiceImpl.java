3c4b9c3d0e678f37abe0d9ba24938791fa957136
yassine sahyane <62175613+Yassinesahyane89@users.noreply.github.com>
2023-12-19 18:36:58 +0100

    move CompetitionServiceImpl to service.Impl package

--- src/main/java/com/example/demo/service/Impl/CompetitionServiceImpl.java
+++ src/main/java/com/example/demo/service/Impl/CompetitionServiceImpl.java
@@ -1,9 +1,10 @@
-package com.example.demo.service;
+package com.example.demo.service.Impl;
 
 import com.example.demo.entity.Competition;
 import com.example.demo.entity.Member;
 import com.example.demo.entity.Ranking;
 import com.example.demo.repository.CompetitionRepository;
+import com.example.demo.service.CompetitionService;
 
 import java.util.List;
 
