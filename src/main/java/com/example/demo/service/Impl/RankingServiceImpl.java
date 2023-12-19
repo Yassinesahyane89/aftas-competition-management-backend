b4bdb24d0436fb1ebf0ef30235a97a8416d3886f
yassine sahyane <62175613+Yassinesahyane89@users.noreply.github.com>
2023-12-19 19:56:21 +0100

    inject RankingRepository in RankingServiceImpl

--- src/main/java/com/example/demo/service/Impl/RankingServiceImpl.java
+++ src/main/java/com/example/demo/service/Impl/RankingServiceImpl.java
@@ -1,11 +1,17 @@
 package com.example.demo.service.Impl;
 
 import com.example.demo.entity.Ranking;
+import com.example.demo.repository.RankingRepository;
 import com.example.demo.service.RankingService;
 
 import java.util.List;
 
 public class RankingServiceImpl implements RankingService {
+    private final RankingRepository rankingRepository;
+
+    public RankingServiceImpl(RankingRepository rankingRepository) {
+        this.rankingRepository = rankingRepository;
+    }
     @Override
     public Ranking getRankingByCompetitionCodeAndMemberNumber(String competitionCode, Long memberNumber) {
         return null;
