38c4f5503100f9514f19389a802bb0bbcac86441
yassine sahyane <62175613+Yassinesahyane89@users.noreply.github.com>
2023-12-19 19:47:17 +0100

    refactoring code of RankingRepository

--- src/main/java/com/example/demo/repository/RankingRepository.java
+++ src/main/java/com/example/demo/repository/RankingRepository.java
@@ -1,7 +1,9 @@
 package com.example.demo.repository;
 
+import com.example.demo.entity.RankId;
 import com.example.demo.entity.Ranking;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface RankingRepository extends JpaRepository<Ranking, Long> {
+public interface RankingRepository extends JpaRepository<Ranking, RankId> {
+
 }
