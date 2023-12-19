f9cc739f975707dc059a189b184d0e993197b5cc
yassine sahyane <62175613+Yassinesahyane89@users.noreply.github.com>
2023-12-19 10:26:27 +0100

    inject MemberService in MemberController

--- src/main/java/com/example/demo/web/rest/controller/MemberController.java
+++ src/main/java/com/example/demo/web/rest/controller/MemberController.java
@@ -1,5 +1,6 @@
 package com.example.demo.web.rest.controller;
 
+import com.example.demo.service.MemberService;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,5 +9,9 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/v1/members")
 @CrossOrigin("*")
 public class MemberController {
+    private final MemberService memberService;
 
+    public MemberController(MemberService memberService) {
+        this.memberService = memberService;
+    }
 }
