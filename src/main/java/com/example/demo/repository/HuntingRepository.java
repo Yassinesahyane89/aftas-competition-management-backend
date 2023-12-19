6f24bb2b17e9427a1d575e0b814f30c5ddd5c36f
yassine sahyane <62175613+Yassinesahyane89@users.noreply.github.com>
2023-12-19 22:33:44 +0100

    add findByCompetitionCodeAndMemberMembershipNumberAndFishId methode in HuntingRepository

--- src/main/java/com/example/demo/repository/HuntingRepository.java
+++ src/main/java/com/example/demo/repository/HuntingRepository.java
@@ -4,4 +4,5 @@ import com.example.demo.entity.Hunting;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface HuntingRepository extends JpaRepository<Hunting, Long> {
+    Hunting findByCompetitionCodeAndMemberMembershipNumberAndFishId(String code, Long membershipNumber, Long fishId);
 }
