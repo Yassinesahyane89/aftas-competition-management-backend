1c85de1b4df1ed3b84049f22e8ac0368d94f62d9
yassine sahyane <62175613+Yassinesahyane89@users.noreply.github.com>
2023-12-19 18:38:28 +0100

    add getCompetitionById logic

--- src/main/java/com/example/demo/service/Impl/CompetitionServiceImpl.java
+++ src/main/java/com/example/demo/service/Impl/CompetitionServiceImpl.java
@@ -3,6 +3,7 @@ package com.example.demo.service.Impl;
 import com.example.demo.entity.Competition;
 import com.example.demo.entity.Member;
 import com.example.demo.entity.Ranking;
+import com.example.demo.handler.exception.ResourceNotFountException;
 import com.example.demo.repository.CompetitionRepository;
 import com.example.demo.service.CompetitionService;
 
@@ -16,7 +17,7 @@ public class CompetitionServiceImpl implements CompetitionService {
     }
     @Override
     public Competition getCompetitionById(Long id) {
-        return null;
+        return competitionRepository.findById(id).orElseThrow(() -> new ResourceNotFountException("Competition id " + id + " not found"));
     }
 
     @Override
