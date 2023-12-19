a16645aa380cc6005d5f2bdcab5eae812c8ac175
yassine sahyane <62175613+Yassinesahyane89@users.noreply.github.com>
2023-12-19 22:05:07 +0100

    inject HuntingRepository to HuntingServiceImpl

--- src/main/java/com/example/demo/service/Impl/HuntingServiceImpl.java
+++ src/main/java/com/example/demo/service/Impl/HuntingServiceImpl.java
@@ -1,11 +1,17 @@
 package com.example.demo.service.Impl;
 
 import com.example.demo.entity.Hunting;
+import com.example.demo.repository.HuntingRepository;
 import com.example.demo.service.HuntingService;
 
 import java.util.List;
 
 public class HuntingServiceImpl implements HuntingService {
+    private final HuntingRepository huntingRepository;
+
+    public HuntingServiceImpl(HuntingRepository huntingRepository) {
+        this.huntingRepository = huntingRepository;
+    }
     @Override
     public Hunting getHuntingById(Long id) {
         return null;
