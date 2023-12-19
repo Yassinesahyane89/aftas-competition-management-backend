a47c00ea84481df02f095e8e8fd7af08566ba7c3
yassine sahyane <62175613+Yassinesahyane89@users.noreply.github.com>
2023-12-19 21:13:12 +0100

    add new methode findAllByCompetitionCode and findAllNotInCompetition in MemberRepository

--- src/main/java/com/example/demo/repository/MemberRepository.java
+++ src/main/java/com/example/demo/repository/MemberRepository.java
@@ -12,4 +12,17 @@ public interface MemberRepository extends JpaRepository<Member, Long> {
             "SELECT * FROM member WHERE membership_number = :searchTerm " +
             "OR first_name LIKE %:searchTerm% OR family_name LIKE %:searchTerm%", nativeQuery = true)
     List<Member> findByMembershipNumberOrFirstNameOrFamilyName(@Param("searchTerm") String searchTerm);
+
+    // get all members that are in a competition
+    @Query( value =
+            "SELECT * FROM member WHERE membership_number IN " +
+            "(SELECT member_membership_number FROM ranking WHERE competition_code = :competitionCode)", nativeQuery = true)
+    List<Member> findAllByCompetitionCode(@Param("competitionCode") String competitionCode);
+
+    // get all members that are not in a competition
+    @Query( value =
+            "SELECT * FROM member WHERE membership_number NOT IN " +
+            "(SELECT member_membership_number FROM ranking WHERE competition_code = :competitionCode)", nativeQuery = true)
+    List<Member> findAllNotInCompetition(@Param("competitionCode") String competitionCode);
+
 }
