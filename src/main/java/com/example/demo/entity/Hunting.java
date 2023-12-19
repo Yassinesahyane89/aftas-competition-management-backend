3fbb87e1d2700c66afaf6bdea9349e7ab27e3464
yassine sahyane <62175613+Yassinesahyane89@users.noreply.github.com>
2023-12-19 11:56:24 +0100

    create and init Hunting entity

---
+++ src/main/java/com/example/demo/entity/Hunting.java
@@ -0,0 +1,22 @@
+package com.example.demo.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@EntityListeners(AuditingEntityListener.class)
+public class Hunting {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    private Integer numberOfHunters;
+}
