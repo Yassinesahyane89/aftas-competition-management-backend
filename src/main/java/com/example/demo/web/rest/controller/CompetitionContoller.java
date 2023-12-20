0d5907501a02e088fa36c2d70b1cb73922d01fe3
yassine sahyane <62175613+Yassinesahyane89@users.noreply.github.com>
2023-12-20 11:46:19 +0100

    add getAllCompetitions method logic in CompetitionContoller

--- src/main/java/com/example/demo/web/rest/controller/CompetitionContoller.java
+++ src/main/java/com/example/demo/web/rest/controller/CompetitionContoller.java
@@ -1,9 +1,17 @@
 package com.example.demo.web.rest.controller;
 
+import com.example.demo.entity.Competition;
+import com.example.demo.handler.response.ResponseMessage;
 import com.example.demo.service.CompetitionService;
+import com.example.demo.web.DTO.response.CompetitionResponseDTO;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/competitions")
@@ -13,5 +21,20 @@ public class CompetitionContoller {
 
     public CompetitionContoller(CompetitionService competitionService) {
         this.competitionService = competitionService;
+    }
+
+    //get all competitions
+    @GetMapping
+    public ResponseEntity getAllCompetitions() {
+        List<Competition> competitions = competitionService.getAllCompetitions();
+        if(competitions.isEmpty()) {
+            return ResponseMessage.notFound("Competition not found");
+        }else {
+            List<CompetitionResponseDTO> competitionResponseDTOS = new ArrayList<>();
+            for(Competition competition : competitions) {
+                competitionResponseDTOS.add(CompetitionResponseDTO.fromCompetition(competition));
+            }
+            return ResponseMessage.ok(competitionResponseDTOS, "Success");
+        }
     }
 }
