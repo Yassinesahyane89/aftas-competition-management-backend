8d4563d2085f4d5f95a251447d52bdf0f8bb6730
yassine sahyane <62175613+Yassinesahyane89@users.noreply.github.com>
2023-12-20 11:48:15 +0100

    add addCompetition method logic in CompetitionContoller

--- src/main/java/com/example/demo/web/rest/controller/CompetitionContoller.java
+++ src/main/java/com/example/demo/web/rest/controller/CompetitionContoller.java
@@ -3,12 +3,11 @@ package com.example.demo.web.rest.controller;
 import com.example.demo.entity.Competition;
 import com.example.demo.handler.response.ResponseMessage;
 import com.example.demo.service.CompetitionService;
+import com.example.demo.web.DTO.request.CompetitionRequestDTO;
 import com.example.demo.web.DTO.response.CompetitionResponseDTO;
+import jakarta.validation.Valid;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -46,6 +45,17 @@ public class CompetitionContoller {
             return ResponseMessage.notFound("Competition not found");
         }else {
             return ResponseMessage.ok(CompetitionResponseDTO.fromCompetition(competition), "Success");
+        }
+    }
+
+    //add competition
+    @PostMapping
+    public ResponseEntity addCompetition(@Valid @RequestBody CompetitionRequestDTO competitionRequestDTO) {
+        Competition newCompetition = competitionService.addCompetition(competitionRequestDTO.toCompetition());
+        if(newCompetition == null) {
+            return ResponseMessage.badRequest("Competition not created");
+        }else {
+            return ResponseMessage.created(CompetitionResponseDTO.fromCompetition(newCompetition), "Competition created successfully");
         }
     }
 }
